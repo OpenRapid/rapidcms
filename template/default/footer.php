402d524836cd995097569e02a9d9636226718899
SUDA编程 <100057379+suda-code@users.noreply.github.com>
2023-12-30 12:01:24 +0800

    前进！Node.js

--- template/default/footer.php
+++
@@ -1,2 +0,0 @@
-<script src="../../../../../../resource/js/mtu.min.js"></script>
-<script src="../../../../../../resource/js/mdui.min.js"></script>
--- template/default/header.php
+++
@@ -1,6 +0,0 @@
-<link rel="stylesheet" href="../../../../../../resource/css/mdui.min.css"/>
-<link rel="stylesheet" href="../../../../../../resource/css/mtu.min.css">
-<link rel="stylesheet" href="../../../../resource/css/style.css">
-<link rel="stylesheet" href="../../../../../template/default/theme.css">
-
-    
